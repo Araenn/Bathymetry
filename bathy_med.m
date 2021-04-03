c4bacef700c018d7f7ee51594d181c8216a1c2d0
Léa Yeromonahos <51425332+Araenn@users.noreply.github.com>
2021-04-03 19:15:21 +0200

    Added comments

--- bathy_med.m
+++ bathy_med.m
@@ -1,25 +1,23 @@
 clear;
-ncdisp('bathy.nc')
+ncdisp('bathy.nc') %show informations contained in your file
 
 %read from the .nc file
-Lon = ncread('bathy.nc', 'nav_lon');
-Lat = ncread('bathy.nc', 'nav_lat');
-B = ncread('bathy.nc', 'Bathymetry');
-x = ncread('bathy.nc', 'x');
-y = ncread('bathy.nc', 'y');
+Lon = ncread('bathy.nc', 'nav_lon'); %longitude
+Lat = ncread('bathy.nc', 'nav_lat'); %latitude
+B = ncread('bathy.nc', 'Bathymetry'); %depth
 
 %create map matrix
-col3 = (0:0.0001:1)';
-col12 = zeros(numel(col3), 2);
-t = [1 1 1]; %the white
-map = [col12,col3;t];
+col3 = (0:0.0001:1)'; %blue row color 
+col12 = zeros(numel(col3), 2); %create the first 2 rows composed of zeros, in order to create a RGB matrix
+t = [1 1 1]; %in order to color the land in white
+map = [col12,col3;t]; %create the RGB matrix as [0 0 0; 0 0 0.0001; ... ; 1 1 1] 
 
 %plot the data
-colormap(map)
-pcolor(Lon, Lat, -B); 
+colormap(map) %set the colors
+pcolor(Lon, Lat, -B); %plot the colors of Depth in function of longitude and latitude. As Depth is decreasing, don't forget the "-"
 title('Bathymetric data of the French Mediterranean')
 xlabel('Longitude')
 ylabel('Latitude')
-shading flat;
-colorbar
+shading flat; %without, all the datas cannot be plotted correctly (it will give you a black square)
+colorbar %add the "legend" for the depth
 ylabel(colorbar, 'Depth')
